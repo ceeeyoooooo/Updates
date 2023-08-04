65a7a0801459f94ab5cfbcb555d7329c2fd5bc80
yo <109927456+ceeeyoooooo@users.noreply.github.com>
2023-08-04 02:02:29 +0200

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,18 @@
+	UpdateLogs = {
+		"I_This is a beta testing build!",
+        "I_Use new features at your own risk.",
+		"I_Updates are sorted by time of release.",
+		"G_Additions:",
+		'G_New Loader System',
+		"G_Readded old auto time method for testing",
+		'G_Added new auto guard tracker',
+		"Y_Fixes/Improvements:",
+		"Y_Fixed dunk self improver",
+		'Y_Fixed manual auto time',
+        "Y_Renamed bar-optimizedtest",
+		"S_To Do List, Next Update:",
+		"S_Legit Auto Guard (Guard assist)",
+		"S_Remake auto time section",
+		"S_Auto time contest limit",
+		"S_Adjust manual auto time"
+	}
